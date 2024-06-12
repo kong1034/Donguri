96a3b5ec3c04c99706c2b09af5cc6243894ac25a
yuree <yureeee89@gmail.com>
2024-06-12 19:05:35 +0900

    [Docs] change board package name

--- src/main/java/com/donguri/board/BoardDetailC.java
+++ src/main/java/com/donguri/board/BoardDetailC.java
@@ -1,4 +1,4 @@
-package com.donguri_board;
+package com.donguri.board;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,13 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/BoardMakeC")
-public class BoardMakeC extends HttpServlet {
+@WebServlet("/BoardDetailC")
+public class BoardDetailC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 		request.setCharacterEncoding("utf-8");
-		request.setAttribute("content", "/jsp/board/board_make.jsp");
+		
+		DAOBoard.getOneBoardList(request);
+		request.setAttribute("content", "/jsp/board/board_detail.jsp");
 		request.getRequestDispatcher("/jsp/board/board_main.jsp").forward(request, response);
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/com/donguri/board/BoardEpilogueC.java
+++ src/main/java/com/donguri/board/BoardEpilogueC.java
@@ -1,22 +1,19 @@
-package com.donguri_board;
+package com.donguri.board;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/BoardC")
-public class BoardC extends HttpServlet {
+@WebServlet("/BoardEpilogueC")
+public class BoardEpilogueC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		
-		DAOBoard.getAllBoardList(request);
-		request.setAttribute("content", "/jsp/board/board.jsp");
+		request.setAttribute("content", "/jsp/board/board_epilogue.jsp");
 		request.getRequestDispatcher("/jsp/board/board_main.jsp").forward(request, response);
+	
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
