71045729bc716e8f6e64bf55c3e75b7741f6ed68
KONG1034 <kyh93054@naver.com>
2024-06-13 15:21:07 +0900

    [ADD] info_all.jsp html/css/js/servlet

---
+++ src/main/webapp/js/info/info_child.js
@@ -0,0 +1,25 @@
+function goSite(num) {
+	if (num == 1) {
+		window.open("https://ecolife-coco.com/", "_blank");
+	} else if (num == 2) {
+		window.open("https://www.osaka-kodomoshien.com/", "_blank");
+	} else if (num == 3) {
+		window.open("https://jspedanes.smoosy.atlas.jp/ja/facility", "_blank");
+	} else if (num == 4) {
+		window.open("https://litalico-c.jp/job_search", "_blank");
+	} else if (num == 5) {
+		window.open("https://www.cfa.go.jp/policies/shougaijishien", "_blank");
+	} else if (num == 6) {
+		window.open("https://www.mcfh.or.jp/", "_blank");
+	} else if (num == 7) {
+		window.open("http://www.rehab.go.jp/ddis/", "_blank");
+	} else if (num == 8) {
+		window.open("https://www.midwife.or.jp/about/index.html", "_blank");
+	} else if (num == 9) {
+		window.open("https://iko-yo.net/", "_blank");
+	} else if (num == 10) {
+		window.open("https://tokyo-eventplus.com/", "_blank");
+	} else if (num == 11) {
+		window.open("https://www.zenbo.org/", "_blank");
+	}
+}
