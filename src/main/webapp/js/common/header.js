0e0452bc8beffd1cdbb7b90f6546ccd5fb885a97
minseung <winca1122@naver.com>
2024-06-19 18:14:13 +0900

    [Add] Add Header include function (HeaderC, header.js, header.jsp, loginProfile)

--- src/main/webapp/js/common/header.js
+++ src/main/webapp/js/common/header.js
@@ -1,15 +1,4 @@
 $(function () {
-  // Jauery functions for header interactions can be added here
-
-  /* unlogged user viewer  */
-  var loginChk = '<%=request.getAttribute("loginChk")%>';
-
-  if (loginChk === "login") {
-    document.querySelectorAll("#hidden").forEach(function (span) {
-      span.style.display = "none";
-    });
-  }
-
   /* logo img animation */
   $(".header_logo_img").css("transform", "translateX(81%) rotate(1110deg)");
   $(".header_logo_img").css("transition", "all 3s ease");
--- src/main/webapp/jsp/sign/loginProfile.jsp
+++ src/main/webapp/jsp/sign/loginProfile.jsp
@@ -7,13 +7,12 @@
 <title>Insert title here</title>
 </head>
 <body>
-<form action=""></form>
 	<img src="https://item.kakaocdn.net/do/296cc3e891afb5542018b43229eb30ccac8e738cb631e72fdb9a96b36413984e"
-	 onclick="jsp/sign/signup_done.jsp" style=" 
+	style=" 
 	width: 2.5em;
     height: 2.5em;
     border: 1px solid black;
-    border-radius: 50%;">
-    <input type="hidden" value="${loginChk }">
+    border-radius: 50%;"
+    onclick="location.href='MyPageC?result=${loginChk}'">
 </body>
 </html>
