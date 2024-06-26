2c6857c47ea2e747f8386e9f06bc306588736803
yuree <yureeee89@gmail.com>
2024-06-26 19:47:05 +0900

    [Add] add BoardSearchC servlet

--- src/main/java/com/donguri/board/BoardSearchC.java
+++ src/main/java/com/donguri/board/BoardSearchC.java
@@ -1,28 +1,24 @@
 package com.donguri.board;
 
 import java.io.IOException;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/BoardC")
-public class BoardC extends HttpServlet {
+@WebServlet("/BoardSearchC")
+public class BoardSearchC extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("utf-8");
-		
-		DAOBoard.DAOB.getAllBoardList(request);
+		DAOBoard.DAOB.search(request);
 		DAOBoard.DAOB.paging(1, request);
 		
 		request.setAttribute("contentPage", "/jsp/board/board.jsp");
 		request.getRequestDispatcher("index.jsp").forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 	}
 
 }
