4735c54bb5d7a9c867cae26fd79d944a65ac0f09
yuree <yureeee89@gmail.com>
2024-07-01 10:18:20 +0900

    [Feat] BoardMake update function

---
+++ src/main/java/com/donguri/board/BoardUpdateC.java
@@ -0,0 +1,29 @@
+package com.donguri.board;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/BoardUpdateC")
+public class BoardUpdateC extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		DAOBoard.DAOB.getOneBoardList(request);
+		
+		request.setAttribute("contentPage", "/jsp/board/board_make_update.jsp");
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		DAOBoard.DAOB.updateBoard(request);
+		
+		response.sendRedirect("BoardC");
+	}
+
+}
