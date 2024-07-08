e9721c19b71b044237d38eca6700d890270aa842
yuree <yureeee89@gmail.com>
2024-07-08 13:18:33 +0900

    [Fix] Info_older image error

--- src/main/webapp/js/info/info.js
+++ src/main/webapp/js/info/info.js
@@ -64,3 +64,30 @@ function goSite(num) {
 		window.open("https://www.zenbo.org/", "_blank");
 	}
 }
+function goSite2(num) {
+	if (num == 1) {
+		window.open("https://nenrin.or.jp/", "_blank");
+	} else if (num == 2) {
+		window.open("https://www.wam.go.jp/content/wamnet/pcpub/top/", "_blank");
+	} else if (num == 3) {
+		window.open("https://www.n-helper.com/", "_blank");
+	} else if (num == 4) {
+		window.open("https://www.kyoukaikenpo.or.jp/", "_blank");
+	} else if (num == 5) {
+		window.open("https://www.sawayakazaidan.or.jp/", "_blank");
+	} else if (num == 6) {
+		window.open("http://www.zenrouren.com/", "_blank");
+	} else if (num == 7) {
+		window.open("https://wabas.sakura.ne.jp/", "_blank");
+	} else if (num == 8) {
+		window.open("https://www.espa.or.jp/", "_blank");
+	} else if (num == 9) {
+		window.open("https://www.niid.go.jp/eiken/", "_blank");
+	} else if (num == 10) {
+		window.open("https://www.gpif.go.jp/", "_blank");
+	} else if (num == 11) {
+		window.open("https://www.kaigo-shien-kyokai.or.jp/", "_blank");
+	} else if (num == 12) {
+		window.open("https://www.roushikyo.or.jp/", "_blank");
+	}
+}
